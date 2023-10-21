a3489120c0e941e5e322c82199e447cc5bcc264b
chetan <chetan@gmail.com>
2023-10-21 13:56:49 +0530

    change

--- backend/api/controller/leaves/get.js
+++ backend/api/controller/leaves/get.js
@@ -5,12 +5,12 @@ const getTotalLeavesData = ({ leavesModel }, { config }) =>
   async (req, res, next) => {
     const { emp_id } = req.user
     try {
-      const findleaveQuery = `select doj from public."employee_profiles" WHERE emp_id = ?;`;
-      const leaves = await db.sequelize.query(findleaveQuery, {
+      const findDojQuery = `select doj from public."employee_profiles" WHERE emp_id = ?;`;
+      const _doj = await db.sequelize.query(findDojQuery, {
         replacements: [emp_id],
         type: QueryTypes.SELECT,
       });
-      if (!leaves || leaves.length == 0) {
+      if (!_doj || _doj.length == 0) {
         return res.json({
           status: false,
           code: 200,
@@ -18,14 +18,45 @@ const getTotalLeavesData = ({ leavesModel }, { config }) =>
           message: "No data found",
         })
       }
+
       let result = {
-        total_leaves:'',
-        applied_leaves:'',
-        pending_leaves:''
-    }
-    let doj = moment(leaves[0].doj).month()
-    let curr = moment().month()
-    result.total_leaves = (curr-doj)*1.5
+        total_leaves: '',
+        applied_leaves: '',
+        pending_leaves: ''
+      }
+      var startDate = moment(_doj[0].doj);
+      if(Number(_doj[0].doj.split('-')[0])<2023){
+        startDate = moment('2023-01-01')
+      }
+      var endDate = moment();
+      var betweenMonths = [];
+      var noOfMonths = 0;
+      const findMonthlyLeave = `select month,year,applied_leaves from public.attendances where emp_id = ?;`;
+      const app_leaves = await db.sequelize.query(findMonthlyLeave, {
+        replacements: [emp_id],
+        type: QueryTypes.SELECT,
+      });
+      let all_app_leaves = 0
+      if (app_leaves && app_leaves.length > 0) {
+        all_app_leaves = app_leaves.reduce((acc, e) => {
+          return acc + Number(e.applied_leaves)
+        }, 0)
+      }
+      if (startDate < endDate) {
+        var date = startDate.startOf('month');
+        while (date < endDate.endOf('month')) {
+          betweenMonths.push(date.format('YYYY-MM'));
+          date.add(1, 'month');
+        }
+      }
+      if(moment(startDate).date()>15){
+        noOfMonths = betweenMonths.length-1;
+      }else{
+        noOfMonths = betweenMonths.length;
+      }
+      result.total_leaves = noOfMonths * 1.5;
+      result.applied_leaves = all_app_leaves;
+      result.pending_leaves = (noOfMonths * 1.5) - Number(all_app_leaves);
       return res.json({
         status: true,
         code: 200,
@@ -38,4 +69,4 @@ const getTotalLeavesData = ({ leavesModel }, { config }) =>
     }
   }
 
-module.exports = {getTotalLeavesData}
+module.exports = { getTotalLeavesData }
